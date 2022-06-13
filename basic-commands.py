90a5b24250cdbbb58ca2c3d63f1267ba90cec531
matthew-botten <86299176+matthew-botten@users.noreply.github.com>
2022-06-13 23:17:28 +0100

    Update basic-commands.py

--- basic-commands.py
+++ basic-commands.py
@@ -2,4 +2,5 @@ def add(value,interest,years):
     rate = 1 + (interest/100)
     endValue = value * rate**years
     return endValue
-print("This is a compound interest calculator\n", add(int(input("Enter the original value (£): ")),int(input("Enter the interest rate (%): ")),int(input("Enter the years of interest: "))) )
+print("This is a compound interest calculator\n")
+print( add( int(input("Enter the original value (£): ")),int(input("Enter the interest rate (%): ")),int(input("Enter the years of interest: ")) )  )
