721a4fe4f65f702e45e9f4d45e1bdbb5790f7a01
Matthew-Botten <github@matthewbotten.com>
2022-06-13 23:08:30 +0100

    created basic compound interest calculator

--- basic-commands.py
+++ basic-commands.py
@@ -1,4 +1,5 @@
-print("Hello World")
-def add(num1,num2):
-  return num1+num2
-print(add(int(input("Enter No1: ")),int(input("Enter No2: "))))
+def add(value,interest,years):
+    rate = 1 + (interest/100)
+    endValue = value * rate**years
+    return endValue
+print("This is a compound interest calculator\n", add(int(input("Enter the original value (£): ")),int(input("Enter the interest rate (%): ")),int(input("Enter the years of interest: ") )
