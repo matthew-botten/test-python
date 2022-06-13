c7c533845a9b42e5b9ec220bbd712c3ab5a23334
Matthew-Botten <github@matthewbotten.com>
2022-06-13 23:22:32 +0100

    added rounding

--- basic-commands.py
+++ basic-commands.py
@@ -1,6 +1,6 @@
 def add(value,interest,years):
     rate = 1 + (interest/100)
     endValue = value * rate**years
-    return endValue
+    return round(endValue,2)
 print("This is a compound interest calculator\n")
-print( add( int(input("Enter the original value (£): ")),int(input("Enter the interest rate (%): ")),int(input("Enter the years of interest: ")) )  )
+print("£" + str(add( int(input("Enter the original value (£): ")),int(input("Enter the interest rate (%): ")),int(input("Enter the years of interest: ")) ))  )
