7da7f2fd1d984de755ef3a0b930b42e1a5017ca2
Matthew-Botten <github@matthewbotten.com>
2022-06-23 20:34:57 +0100

    calculate and then plot a regression line

--- basic-machine-learning.py
+++ basic-machine-learning.py
@@ -41,3 +41,22 @@ x = numpy.random.normal(100.0, 25.0, 10000)
 y = numpy.random.normal(5.0, 3.0, 10000)
 plt.scatter(x, y)
 plt.show()
+
+
+from scipy import stats
+#plot a scatter graph
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+#calculate regression line
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+#function that map's each x coordinate to a point on the regression line
+def myfunc(x):
+  return slope * x + intercept
+#runs each value through the function to create a new array
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+#plot a line
+plt.plot(x, mymodel)
+plt.show()
