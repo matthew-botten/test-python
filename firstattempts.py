0773af2fb00d003831506c5b44b4a424ab84bd8b
Matthew-Botten <github@matthewbotten.com>
2022-06-28 22:04:08 +0100

    no change

--- firstattempts.py
+++ firstattempts.py
@@ -1,2 +1,3 @@
 #This is where I will try to start coding my own basic applications of the
 # machine learning content I learnt from w3schools' course
+print("Nothing here yet, this file does nothing!")
