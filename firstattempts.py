88897514afcd989045f25bb9b898ad11c28dee14
Matthew-Botten <github@matthewbotten.com>
2022-06-29 22:49:25 +0100

    comparing a linear and polynomial regression line on one graph

--- firstattempts.py
+++ firstattempts.py
@@ -1,3 +1,23 @@
 #This is where I will try to start coding my own basic applications of the
 # machine learning content I learnt from w3schools' course
 print("Nothing here yet, this file does nothing!")
+
+import numpy
+from scipy import stats
+import matplotlib.pyplot as plt
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 5))
+myline = numpy.linspace(min(x), max(x), 1000)
+plt.plot(myline, mymodel(myline))
+
+plt.show()
