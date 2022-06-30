e46ecad7b6916103f9eaf83dd3ab5ddaa989a569
Matthew-Botten <github@matthewbotten.com>
2022-06-30 22:37:26 +0100

    rudimentary training and testing a regression curve

--- w3schools-freeMLpythonCourse-basic.py
+++ w3schools-freeMLpythonCourse-basic.py
@@ -89,3 +89,23 @@ regr.fit(X.values, y.values)
 predictedCO2 = regr.predict([[2300, 1300]])
 print(predictedCO2)
 print(regr.coef_)
+
+
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(train_y, mymodel(train_x))
+
+print(r2)
