3bde797c7454072bb0cb1dc0667dd779bef1f21f
matthew-botten <86299176+matthew-botten@users.noreply.github.com>
2022-07-03 22:19:05 +0100

    Update firstattempts.py

--- firstattempts.py
+++ firstattempts.py
@@ -1,6 +1,5 @@
 #This is where I will try to start coding my own basic applications of the
 # machine learning content I learnt from w3schools' course
-print("Nothing here yet, this file does nothing!")
 
 import numpy
 from scipy import stats
