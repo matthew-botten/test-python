49249d835667f2ba87cd32af35e515153a44af45
Matthew-Botten <github@matthewbotten.com>
2022-07-11 21:38:56 +0100

    learning how a dendrogram works

--- w3schools-freeMLpythonCourse-basic.py
+++ w3schools-freeMLpythonCourse-basic.py
@@ -216,9 +216,18 @@ print({"Accuracy":Accuracy,"Precision":Precision,"Sensitivity_recall":Sensitivit
 #unsupervised learning
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
 
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+data = list(zip(x, y))
 
-plt.scatter(x, y)
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+hierarchical_cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+labels = hierarchical_cluster.fit_predict(data)
+
+plt.scatter(x, y, c=labels)
+
 plt.show()
