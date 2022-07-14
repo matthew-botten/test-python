dfcb9cd2473d9cc51cdf6ae66c5ad31e5ad78bcb
Matthew-Botten <github@matthewbotten.com>
2022-07-14 23:02:02 +0100

    started something new

--- w3schools-freeMLpythonCourse-basic.py
+++ w3schools-freeMLpythonCourse-basic.py
@@ -212,7 +212,7 @@ F1_score = metrics.f1_score(actual, predicted)
 print({"Accuracy":Accuracy,"Precision":Precision,"Sensitivity_recall":Sensitivity_recall,"Specificity":Specificity,"F1_score":F1_score})
 """ 
 
-
+"""
 #unsupervised learning
 import numpy as np
 import matplotlib.pyplot as plt
@@ -238,3 +238,19 @@ labels = hierarchical_cluster.fit_predict(data)
 plt.scatter(x, y, c=labels)
 
 plt.show()
+"""
+
+
+import numpy
+from sklearn import linear_model
+
+#Reshaped for Logistic function.
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
+
+#predict if tumor is cancerous where the size is 3.46mm:
+predicted = logr.predict(numpy.array([3.46]).reshape(-1,1))
+print(predicted)
