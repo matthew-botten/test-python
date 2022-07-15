ee9468cd35937be14d00a99c2342ab4b8a167221
Matthew-Botten <github@matthewbotten.com>
2022-07-15 22:59:48 +0100

    fixed the bit that wasn't working yesterday

--- w3schools-freeMLpythonCourse-basic.py
+++ w3schools-freeMLpythonCourse-basic.py
@@ -254,3 +254,7 @@ logr.fit(X,y)
 #predict if tumor is cancerous where the size is 3.46mm:
 predicted = logr.predict(numpy.array([3.46]).reshape(-1,1))
 print(predicted)
+log_odds = logr.coef_ 
+odds = numpy.exp(log_odds)
+
+print(odds)
