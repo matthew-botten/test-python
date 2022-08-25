f47314e945acfe43d1cf7af9ad59068a6a71c64c
matthew-botten <86299176+matthew-botten@users.noreply.github.com>
2022-08-25 21:21:11 +0200

    Update firstattempts.py

--- firstattempts.py
+++ firstattempts.py
@@ -1,6 +1,7 @@
 #This is where I will try to start coding my own basic applications of the
 # machine learning content I learnt from w3schools' course
 
+from time import sleep
 import numpy
 from scipy import stats
 import matplotlib.pyplot as plt
